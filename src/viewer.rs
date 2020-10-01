c7f87b757cb45a99eac7c70ab80ad6a2b86c4102
YoheiIguchi <matthew.at.muse@gmail.com>
2020-10-01 12:50:18 +0900

    horizontal and vertical scroll

--- src/viewer.rs
+++ src/viewer.rs
@@ -17,6 +17,8 @@ pub struct Viewer {
     stdout: AlternateScreen<termion::raw::RawTerminal<std::io::Stdout>>,
     cursor_x: u16,
     cursor_y: u16,
+    row_offset: u16,
+    column_offset: u16,
     window_size_col: u16,
     window_size_row: u16,
     editor_lines: Vec<EditorLine>,
@@ -40,13 +42,17 @@ impl Viewer {
     fn new() -> Self {
         let stdout = Viewer::enable_raw_mode();
         let (window_size_col, window_size_row) = Viewer::get_window_size();
-        let cursor_x = 1;
-        let cursor_y = 1;
+        let cursor_x = 0;
+        let cursor_y = 0;
+        let row_offset = 0;
+        let column_offset = 0;
 
         Self {
             stdout,
             cursor_x,
             cursor_y,
+            row_offset,
+            column_offset,
             window_size_col,
             window_size_row,
             editor_lines: vec![],
@@ -68,86 +74,83 @@ impl Viewer {
         self.editor_lines = editor_lines;
     }
 
-    fn saturated_add_x(&mut self, x: u16) {
-        if self.cursor_x < self.window_size_col {
-            let cursor_x = self.cursor_x + x;
-            write!(
-                self.stdout,
-                "{}",
-                cursor::Goto(self.cursor_x, self.cursor_y)
-            )
-            .unwrap();
+    fn saturated_add_x(&mut self) {
+        if self.cursor_x < self.get_current_row_length() {
+            self.cursor_x = self.cursor_x + 1;
         }
     }
 
-    fn saturated_substract_x(&mut self, x: u16) {
+    fn saturated_substract_x(&mut self) {
         if 0 < self.cursor_x {
-            let cursor_x = self.cursor_x - x;
-            write!(
-                self.stdout,
-                "{}",
-                cursor::Goto(self.cursor_x, self.cursor_y)
-            )
-            .unwrap();
+            self.cursor_x = self.cursor_x - 1;
         }
     }
 
-    fn saturated_add_y(&mut self, y: u16) {
-        if self.cursor_y < self.window_size_row {
-            let cursor_y = self.cursor_y + y;
-            write!(
-                self.stdout,
-                "{}",
-                cursor::Goto(self.cursor_x, self.cursor_y)
-            )
-            .unwrap();
+    fn saturated_add_y(&mut self) {
+        if self.cursor_y + 1 < self.get_editor_line_length() {
+            self.cursor_y = self.cursor_y + 1;
+            if (self.cursor_x) > self.get_current_row_length() {
+                self.cursor_x = self.get_current_row_length()
+            }
         }
     }
 
-    fn saturated_substract_y(&mut self, y: u16) {
+    fn saturated_substract_y(&mut self) {
         if 0 < self.cursor_y {
-            let cursor_x = self.cursor_y - y;
-            write!(
-                self.stdout,
-                "{}",
-                cursor::Goto(self.cursor_x, self.cursor_y)
-            )
-            .unwrap();
+            self.cursor_y = self.cursor_y - 1;
+            if (self.cursor_x) > self.get_current_row_length() {
+                self.cursor_x = self.get_current_row_length()
+            }
         }
     }
 
     fn editor_process_key_press(&mut self) {
         for c in stdin().keys() {
-            //write!(self.stdout, "{:?}", c);
             self.stdout.flush().unwrap();
             match c {
                 Ok(event::Key::Ctrl('c')) | Ok(event::Key::Ctrl('q')) => break,
                 Ok(event::Key::Left) => {
-                    self.saturated_substract_x(1);
+                    self.saturated_substract_x();
                 }
                 Ok(event::Key::Right) => {
-                    self.saturated_add_x(1);
+                    self.saturated_add_x();
                 }
                 Ok(event::Key::Up) => {
-                    self.saturated_substract_y(1);
+                    self.saturated_substract_y();
                 }
                 Ok(event::Key::Down) => {
-                    self.saturated_add_y(1);
+                    self.saturated_add_y();
                 }
                 _ => {}
             }
+            self.editor_refresh_screen()
         }
     }
 
     fn editor_refresh_screen(&mut self) {
-        write!(self.stdout, "{}{}", clear::All, cursor::Hide).unwrap();
+        self.editor_scroll();
+        write!(self.stdout, "{}", clear::All).unwrap();
+        write!(self.stdout, "{}", cursor::Goto(1, 1)).unwrap();
+
+        self.editor_draw_rows();
+
+        eprintln!(
+            "cursor goto {}: {}. row_offset: {}. editor_line: {}",
+            self.cursor_x,
+            self.cursor_y,
+            self.row_offset,
+            self.editor_lines.len()
+        );
         write!(
             self.stdout,
             "{}",
-            cursor::Goto(self.cursor_x, self.cursor_y)
+            cursor::Goto(
+                self.cursor_x + 1 - self.column_offset,
+                self.cursor_y + 1 - self.row_offset
+            )
         )
         .unwrap();
-        self.editor_draw_rows();
+
         self.stdout.flush().unwrap();
     }
 
@@ -174,10 +177,22 @@ impl Viewer {
         welcome_line
     }
 
+    fn get_editor_line_length(&self) -> u16 {
+        self.editor_lines.len() as u16
+    }
+
+    fn get_current_row_length(&self) -> u16 {
+       self.editor_lines[self.cursor_y as usize]
+            .line
+            .chars()
+            .count() as u16
+    }
+
     fn editor_draw_rows(&mut self) {
         for i in 0..self.window_size_row {
-            if i >= self.editor_lines.len() as u16 {
-                if i == (self.window_size_row / 3) {
+            let file_row = i + self.row_offset as u16;
+            if file_row >= self.get_editor_line_length() {
+                if self.get_editor_line_length() == 0 && i == (self.window_size_row / 3) {
                     let welcome_line = self.get_welcome_line();
                     write!(self.stdout, "{}", welcome_line).unwrap();
                 } else {
@@ -185,13 +200,49 @@ impl Viewer {
                     write!(self.stdout, "{}", line).unwrap();
                 }
             } else {
-                let line = format!("{}", self.editor_lines[i as usize].line);
+                let mut len = self.editor_lines[file_row as usize].line.chars().count() as i16
+                    - self.column_offset as i16;
+                if len < 0 {
+                    len = 0
+                }
+
+                if len > self.window_size_col as i16 {
+                    len = self.window_size_col as i16
+                }
+                let whole_line = format!("{}", self.editor_lines[file_row as usize].line);
+
+                let mut line = String::new();
+                for (i, c) in whole_line.chars().enumerate() {
+                    if i >= self.column_offset as usize
+                        && i < (self.window_size_col + self.column_offset) as usize
+                    {
+                        line.push(c);
+                    }
+                }
+
                 write!(self.stdout, "{}", line).unwrap();
             }
 
             if i < self.window_size_row - 1 {
                 write!(self.stdout, "\r\n").unwrap();
             }
+        }
+    }
+    fn editor_scroll(&mut self) {
+        if self.cursor_y < self.row_offset {
+            self.row_offset = self.cursor_y;
+        }
+
+        if self.cursor_y >= (self.row_offset + self.window_size_row) {
+            self.row_offset = self.cursor_y - self.window_size_row + 1;
+        }
+
+        if self.cursor_x < self.column_offset {
+            self.column_offset = self.cursor_x;
+        }
+
+        if self.cursor_x >= self.column_offset + self.window_size_col {
+            self.column_offset = self.cursor_x - self.window_size_col + 1;
         }
     }
 
